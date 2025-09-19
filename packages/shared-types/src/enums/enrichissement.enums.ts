0745e0403954e876716dee72d601649846b185a9
Samir BENFARES <sbenfares@gmail.com>
2025-09-19 15:24:38 +0200

    feat: add NSP for TVB

--- packages/shared-types/src/enums/enrichissement.enums.ts
+++ packages/shared-types/src/enums/enrichissement.enums.ts
@@ -62,4 +62,5 @@ export enum TrameVerteEtBleue {
   RESERVOIR_BIODIVERSITE = "reservoir-biodiversite",
   CORRIDOR_A_RESTAURER = "corridor-a-restaurer",
   CORRIDOR_A_PRESERVER = "corridor-a-preserver",
+  NE_SAIT_PAS = "ne-sait-pas",
 }
