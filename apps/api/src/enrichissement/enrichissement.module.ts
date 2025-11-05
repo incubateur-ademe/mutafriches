369f4169a7bb1061f5e6f72802b7dc23ec34d001
Samir BENFARES <sbenfares@gmail.com>
2025-11-05 16:45:23 +0100

    feat(enrichissement): module & main service

--- apps/api/src/enrichissement/enrichissement.module.ts
+++ apps/api/src/enrichissement/enrichissement.module.ts
@@ -1,20 +1,41 @@
 import { Module } from "@nestjs/common";
 import { HttpModule } from "@nestjs/axios";
-
-import { EnrichissementController } from "./enrichissement.controller";
-import { EnrichissementService } from "./services/enrichissement.service";
-import { EnrichissementRepository } from "./repositories/enrichissement.repository";
 import { DatabaseModule } from "../shared/database/database.module";
 
-// Import des services externes
+// Controller
+import { EnrichissementController } from "./enrichissement.controller";
+
+// Repository
+import { EnrichissementRepository } from "./repositories/enrichissement.repository";
+
+// Service principal
+import { EnrichissementService } from "./services/enrichissement.service";
+
+// Sous-domaines d'enrichissement
+import { CadastreEnrichissementService } from "./services/cadastre/cadastre-enrichissement.service";
+import { EnergieEnrichissementService } from "./services/energie/energie-enrichissement.service";
+import { TransportEnrichissementService } from "./services/transport/transport-enrichissement.service";
+import { UrbanismeEnrichissementService } from "./services/urbanisme/urbanisme-enrichissement.service";
+import { RisquesNaturelsEnrichissementService } from "./services/risques-naturels/risques-naturels-enrichissement.service";
+import { RisquesTechnologiquesEnrichissementService } from "./services/risques-technologiques/risques-technologiques-enrichissement.service";
+import { GeoRisquesEnrichissementService } from "./services/georisques/georisques-enrichissement.service";
+
+// Calculators
+import { FiabiliteCalculator } from "./services/shared/fiabilite.calculator";
+import { RisquesNaturelsCalculator } from "./services/risques-naturels/risques-naturels.calculator";
+import { RisquesTechnologiquesCalculator } from "./services/risques-technologiques/risques-technologiques.calculator";
+
+// Orchestrateurs
+import { GeoRisquesOrchestrator } from "./services/georisques/georisques.orchestrator";
+
+// Adapters (APIs externes)
+import { CadastreService } from "./adapters/cadastre/cadastre.service";
 import { BdnbService } from "./adapters/bdnb/bdnb.service";
 import { EnedisService } from "./adapters/enedis/enedis.service";
-import { CadastreService } from "./adapters/cadastre/cadastre.service";
-// Import tous les services GeoRisques
 import { RgaService } from "./adapters/georisques/rga/rga.service";
 import { CatnatService } from "./adapters/georisques/catnat/catnat.service";
-import { TriService } from "./adapters/georisques/tri/tri.service";
 import { TriZonageService } from "./adapters/georisques/tri-zonage/tri-zonage.service";
+import { TriService } from "./adapters/georisques/tri/tri.service";
 import { MvtService } from "./adapters/georisques/mvt/mvt.service";
 import { ZonageSismiqueService } from "./adapters/georisques/zonage-sismique/zonage-sismique.service";
 import { CavitesService } from "./adapters/georisques/cavites/cavites.service";
@@ -26,20 +47,40 @@ import { PapiService } from "./adapters/georisques/papi/papi.service";
 import { PprService } from "./adapters/georisques/ppr/ppr.service";
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [HttpModule, DatabaseModule],
   controllers: [EnrichissementController],
   providers: [
-    EnrichissementService,
+    // Repository
     EnrichissementRepository,
-    // Services externes
+
+    // Service principal (orchestrateur)
+    EnrichissementService,
+
+    // Sous-domaines d'enrichissement
+    CadastreEnrichissementService,
+    EnergieEnrichissementService,
+    TransportEnrichissementService,
+    UrbanismeEnrichissementService,
+    RisquesNaturelsEnrichissementService,
+    RisquesTechnologiquesEnrichissementService,
+    GeoRisquesEnrichissementService,
+
+    // Calculators
+    FiabiliteCalculator,
+    RisquesNaturelsCalculator,
+    RisquesTechnologiquesCalculator,
+
+    // Orchestrateurs
+    GeoRisquesOrchestrator,
+
+    // Adapters (APIs externes)
     CadastreService,
     BdnbService,
     EnedisService,
-    // GeoRisques
     RgaService,
     CatnatService,
-    TriService,
     TriZonageService,
+    TriService,
     MvtService,
     ZonageSismiqueService,
     CavitesService,
@@ -50,6 +91,6 @@ import { PprService } from "./adapters/georisques/ppr/ppr.service";
     PapiService,
     PprService,
   ],
-  exports: [EnrichissementService], // Exporte le service
+  exports: [EnrichissementService],
 })
 export class EnrichissementModule {}
