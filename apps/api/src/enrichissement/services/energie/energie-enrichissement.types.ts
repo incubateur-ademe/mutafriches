19b7256797935410ce059c9977eb4e5ebdba8958
Samir BENFARES <sbenfares@gmail.com>
2025-11-05 16:14:45 +0100

    feat(enrichissement): implement energie subdomain

---
+++ apps/api/src/enrichissement/services/energie/energie-enrichissement.types.ts
@@ -0,0 +1,11 @@
+/**
+ * Types pour le sous-domaine energie
+ */
+
+/**
+ * Données de raccordement électrique
+ */
+export interface DonneesRaccordementElectrique {
+  distance: number;
+  typeRaccordement?: string;
+}
