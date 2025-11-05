d322f8d1086cc082edb20c5872b6208babb053c2
Samir BENFARES <sbenfares@gmail.com>
2025-11-05 16:20:10 +0100

    feat(enrichissement): implement urbanisme subdomain (with temporary mock data)

---
+++ apps/api/src/enrichissement/services/urbanisme/urbanisme-enrichissement.types.ts
@@ -0,0 +1,13 @@
+/**
+ * Types pour le sous-domaine urbanisme
+ */
+
+/**
+ * Données de contexte urbain
+ */
+export interface DonneesContexteUrbain {
+  proximiteCommercesServices?: boolean;
+  tauxLogementsVacants?: number;
+  siteEnCentreVille?: boolean;
+  distanceAutoroute?: number;
+}
