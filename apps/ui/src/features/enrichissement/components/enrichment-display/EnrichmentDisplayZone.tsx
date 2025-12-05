dcc8d77ecd0a8dd524d9ce31c27d4342b423fae0
Samir BENFARES <sbenfares@gmail.com>
2025-12-05 14:30:57 +0100

    feat: lien info bulles

--- apps/ui/src/features/enrichissement/components/enrichment-display/EnrichmentDisplayZone.tsx
+++ apps/ui/src/features/enrichissement/components/enrichment-display/EnrichmentDisplayZone.tsx
@@ -85,25 +85,73 @@ export const EnrichmentDisplayZone: React.FC<EnrichmentDisplayZoneProps> = ({
               id="commune"
               label="Commune"
               value={data.commune}
-              tooltip="Récupéré depuis l'API IGN Cadastre : https://apicarto.ign.fr/api/doc/cadastre"
+              tooltip={
+                <>
+                  Récupéré depuis l'API IGN Cadastre :<br />
+                  <a
+                    href="https://apicarto.ign.fr/api/doc/cadastre"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    apicarto.ign.fr/api/doc/cadastre
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="identifiant-parcelle"
               label="Identifiant parcelle"
               value={data.identifiantParcelle}
-              tooltip="Récupéré depuis l'API IGN Cadastre : https://apicarto.ign.fr/api/doc/cadastre"
+              tooltip={
+                <>
+                  Récupéré depuis l'API IGN Cadastre :<br />
+                  <a
+                    href="https://apicarto.ign.fr/api/doc/cadastre"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    apicarto.ign.fr/api/doc/cadastre
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="surface-site"
               label="Surface du site"
               value={data.surfaceParcelle}
-              tooltip="Récupéré depuis l'API IGN Cadastre : https://apicarto.ign.fr/api/doc/cadastre"
+              tooltip={
+                <>
+                  Récupéré depuis l'API IGN Cadastre :<br />
+                  <a
+                    href="https://apicarto.ign.fr/api/doc/cadastre"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    apicarto.ign.fr/api/doc/cadastre
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="surface-batie"
               label="Surface bâtie"
               value={data.surfaceBatie}
-              tooltip="Récupéré depuis l'API BDNB : https://api.bdnb.io/v1/bdnb"
+              tooltip={
+                <>
+                  Récupéré depuis l'API BDNB :<br />
+                  <a
+                    href="https://api.bdnb.io/v1/bdnb"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    api.bdnb.io/v1/bdnb
+                  </a>
+                </>
+              }
             />
           </div>
         </div>
@@ -121,37 +169,97 @@ export const EnrichmentDisplayZone: React.FC<EnrichmentDisplayZoneProps> = ({
               id="site-centre-ville"
               label="Site en centre ville"
               value={data.centreVille}
-              tooltip="Récupéré depuis l'API de l'annuaire du Service public : https://api-lannuaire.service-public.gouv.fr"
+              tooltip={
+                <>
+                  Récupéré depuis l'API de l'annuaire du Service public :<br />
+                  <a
+                    href="https://api-lannuaire.service-public.gouv.fr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    api-lannuaire.service-public.gouv.fr
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="distance-voie-grande-circulation"
               label="Distance voie à grande circulation"
               value={data.distanceAutoroute}
-              tooltip="Récupéré depuis l'API IGN Géoplateforme WFS : https://geoservices.ign.fr/services-web-essentiels"
+              tooltip={
+                <>
+                  Récupéré depuis l'API IGN Géoplateforme WFS :<br />
+                  <a
+                    href="https://geoservices.ign.fr/services-web-essentiels"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    geoservices.ign.fr/services-web-essentiels
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="distance-transport-commun"
               label="Distance au transport en commun"
               value={data.distanceTransportsEnCommun}
-              tooltip="Récupéré depuis l'API Overpass d'OpenStreetMap : https://dev.overpass-api.de/overpass-doc/fr"
+              tooltip={"Pas de données publiques disponibles pour l'instant"}
             />
             <EnrichmentInfoField
               id="proximite-commerces-services"
               label="Proximité des commerces et services"
               value={data.proximiteCommerces}
-              tooltip="Récupéré depuis la base permanente des équipements (BPE) : https://www.data.gouv.fr/fr/datasets/base-permanente-des-equipements-bpe"
+              tooltip={
+                <>
+                  Récupéré depuis la base permanente des équipements (BPE) :<br />
+                  <a
+                    href="https://www.data.gouv.fr/fr/datasets/base-permanente-des-equipements-bpe"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    data.gouv.fr/datasets/base-permanente-des-equipements-bpe
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="distance-raccordement-electrique"
               label="Distance au raccordement électrique"
               value={data.distanceRaccordement}
-              tooltip="Récupéré depuis l'API Enedis : https://data.enedis.fr/api/explore/v2.1/catalog/datasets"
+              tooltip={
+                <>
+                  Récupéré depuis l'API Enedis :<br />
+                  <a
+                    href="https://data.enedis.fr/api/explore/v2.1/catalog/datasets"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    data.enedis.fr/api/explore/v2.1/catalog/datasets
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="taux-logements-vacants"
               label="Taux de logements vacants"
               value={data.tauxLV}
-              tooltip="Récupéré depuis l'API tabulaire de data.gouv.fr : https://www.data.gouv.fr/datasets/logements-vacants-du-parc-prive-en-france-et-par-commune-departement-region"
+              tooltip={
+                <>
+                  Récupéré depuis l'API tabulaire de data.gouv.fr :<br />
+                  <a
+                    href="https://www.data.gouv.fr/datasets/logements-vacants-du-parc-prive-en-france-et-par-commune-departement-region"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    data.gouv.fr/datasets/logements-vacants
+                  </a>
+                </>
+              }
             />
           </div>
         </div>
@@ -169,31 +277,94 @@ export const EnrichmentDisplayZone: React.FC<EnrichmentDisplayZoneProps> = ({
               id="presence-risques-technologiques"
               label="Présence de risques technologiques"
               value={data.risquesTechno}
-              tooltip="Récupéré depuis l'API Georisques : https://www.georisques.gouv.fr/doc-api"
+              tooltip={
+                <>
+                  Récupéré depuis les données de l'API Georisques :<br />
+                  <a
+                    href="https://www.georisques.gouv.fr/citoyen-recherche-map"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    georisques.gouv.fr/doc-api
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="presence-risques-naturels"
               label="Présence de risques naturels"
               value={data.risquesNaturels}
-              tooltip={`Récupéré depuis l'API Georisques : https://www.georisques.gouv.fr/doc-api. En l'absence de la numérisation des plans de prévention des risques, cette donnée est susceptible d'être faussée`}
+              tooltip={
+                <>
+                  Récupéré depuis les données de l'API Georisques :<br />
+                  <a
+                    href="https://www.georisques.gouv.fr/citoyen-recherche-map"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    georisques.gouv.fr/citoyen-recherche-map
+                  </a>
+                  <br /> <br />
+                  En l'absence de la numérisation des plans de prévention des risques, cette donnée
+                  est susceptible d'être faussée
+                </>
+              }
             />
             <EnrichmentInfoField
               id="type-zonage-environnemental"
               label="Type de zonage environnemental"
               value={data.zonageEnviro}
-              tooltip="Données enrichies via les API Carto Nature et GPU de l'IGN"
+              tooltip={
+                <>
+                  Données enrichies via les API Carto Nature et GPU de l'IGN :<br />
+                  <a
+                    href="https://apicarto.ign.fr/api/doc/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    apicarto.ign.fr/api/doc/
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="zonage-reglementaire"
               label="Zonage réglementaire"
               value={data.zonageUrba}
-              tooltip="Données enrichies via les API Carto Nature et GPU de l'IGN"
+              tooltip={
+                <>
+                  Données enrichies via les API Carto Nature et GPU de l'IGN :<br />
+                  <a
+                    href="https://apicarto.ign.fr/api/doc/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    apicarto.ign.fr/api/doc/
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="type-zonage-patrimonial"
               label="Type de zonage patrimonial"
               value={data.zonagePatrimonial}
-              tooltip="Données enrichies via les API Carto Nature et GPU de l'IGN"
+              tooltip={
+                <>
+                  Données enrichies via les API Carto Nature et GPU de l'IGN :<br />
+                  <a
+                    href="https://apicarto.ign.fr/api/doc/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fr-link fr-text--xs"
+                  >
+                    apicarto.ign.fr/api/doc/
+                  </a>
+                </>
+              }
             />
             <EnrichmentInfoField
               id="continuité-ecologique"
