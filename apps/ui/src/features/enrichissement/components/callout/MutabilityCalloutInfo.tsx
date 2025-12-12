3f50cb86f955db3ad93617a6d8ee6795e2017d15
Samir BENFARES <sbenfares@gmail.com>
2025-12-12 16:11:55 +0100

    wip: ui

---
+++ apps/ui/src/features/enrichissement/components/callout/MutabilityCalloutInfo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const MutabilityCalloutInfo: React.FC = () => {
+  return (
+    <div className="fr-callout">
+      <h3 className="fr-callout__title">Qu’est ce que la mutabilité ?</h3>
+      <p className="fr-callout__text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero
+        ultricies aliquet
+      </p>
+    </div>
+  );
+};
